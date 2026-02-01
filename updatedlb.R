88aaf56433119b1e8f00cc7f826ab5f0f7c44bf1
Jashwanth2104 <jashwanthkadali7379@gmail.com>
2026-02-01 20:03:08 +0530

    joins

--- updatedlb.R
+++ updatedlb.R
@@ -3,3 +3,9 @@ library(dplyr)
 setwd("D:\\R\\UpdatedCDISCPilotData\\UpdatedCDISCPilotData\\SDTM")
 data=read_xpt("lb.xpt")
 data
+
+data  %in%
+  group_by(USUBJID) %in
+  mutate(MAXVAL=max(AVAL,na.rm=TRUE))
+
+lb=read_xpt("")
